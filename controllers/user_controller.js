19d36992834ba47c58fe73fed1eee62fb47c6eb9
casf19 <casf19@gmail.com>
2015-08-18 15:34:55 -0300

    Autorización

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,4 @@
-ers = { admin: {id:1, username:"admin", password:"1234"}, 
+var users = { admin: {id:1, username:"admin", password:"1234"}, 
               pepe:  {id:2, username:"pepe",  password:"5678"}
             };
 
